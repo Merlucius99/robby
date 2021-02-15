86c170d9b1678294b3ab3deac5ab8872f7f1bfd6
Merlucius99 <79099880+Merlucius99@users.noreply.github.com>
2021-02-15 18:49:37 +0200

    Create microservo.py

---
+++ microservo.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+from time import sleep
+
+servo_pin = 16
+duty_cycle = 7.5     # Should be the centre for a SG90
+
+# Configure the Pi to use pin names (i.e. BCM) and allocate I/O
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# Create PWM channel on the servo pin with a frequency of 50Hz
+pwm_servo = GPIO.PWM(servo_pin, 50)
+pwm_servo.start(duty_cycle)
+
+for duty_cycle in range(1,13,1):
+            pwm_servo.ChangeDutyCycle(duty_cycle)
+            sleep(3)
+            
+except KeyboardInterrupt:
+    print("CTRL-C: Terminating program.")
+finally:
+    print("Cleaning up GPIO...")
+    GPIO.cleanup()
